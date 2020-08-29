a319da23044817e11eefb5883aaa2b6a3b992248
Mac Mohan Puthran <mohanmac@gmail.com>
2020-08-29 22:54:57 +0530

    Update Train_TensorFlow.py

--- Train_TensorFlow.py
+++ Train_TensorFlow.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from mlflow.sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout, GlobalMaxPooling2D
